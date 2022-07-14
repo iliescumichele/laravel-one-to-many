759c24d74ea785accf6e71da275a912e2d892666
iliescumichele <iliescumichele@gmail.com>
2022-07-14 18:38:16 +0200

    Inserita la gestione delle relazioni nei model Post e Category

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
 
 
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+
+
     protected $fillable = [
         'title',
         'slug',
